b89637852426cc5a6847c8d341ee451b412d40fe
harihardiyan <lorozloraz@gmail.com>
2026-01-17 04:38:04 +0700

    Create __init__.py

---
+++ atomjax/__init__.py
@@ -0,0 +1,5 @@
+from atomjax.solvers import solve_hydrogen_state
+from atomjax.observables import calculate_observables
+
+__version__ = "0.1.0"
+__all__ = ["solve_hydrogen_state", "calculate_observables"]
